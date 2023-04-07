b410ebd5538e1c956bbc174988da400da5f6663f
cloobTech <belkid98@gmail.com>
2023-04-07 06:22:17 -0700

    setting

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Install Nginx if not already installed
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo chown -R ubuntu:ubuntu /data/
+
+# Create a fake HTML file
+echo "<html><head><title>Test Page</title></head><body><p>This is a test page.</p></body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Update Nginx configuration
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
