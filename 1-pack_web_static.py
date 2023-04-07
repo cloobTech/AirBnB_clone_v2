98e2b60d7ce5414aaf6a5d8ad3e454ad042600a3
cloobTech <belkid98@gmail.com>
2023-04-07 10:33:01 -0700

    achieve

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,15 +9,14 @@ from datetime import datetime
 
 def do_pack():
     """generates a .tgz archive"""
-    try:
-        # create the folder if it doesn't exist
-        local("mkdir -p versions")
-        # create the file name
-        now = datetime.now()
-        file_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
-        # create the archive using tar
-        local("tar -czvf versions/{} web_static".format(file_name))
-        # return the path to the archive
-        return "versions/{}".format(file_name)
-    except:
-        return None
+    # create the folder if it doesn't exist
+    local("mkdir -p versions")
+    # create the file name
+    now = datetime.now()
+    file_name = ("web_static_{}{}{}{}{}{}.tgz"
+                 .format(now.year, now.month, now.day,
+                         now.hour, now.minute, now.second))
+    # create the archive using tar
+    local("tar -czvf versions/{} web_static".format(file_name))
+    # return the path to the archive
+    return "versions/{}".format(file_name)
