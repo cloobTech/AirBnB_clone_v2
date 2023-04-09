51f9f4bbcfd55f778ef1447b07ba2b19a3aa0ade
cloobTech <belkid98@gmail.com>
2023-04-09 01:16:04 -0700

    setting

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,11 +5,16 @@
 if ! command -v nginx &> /dev/null; then
     sudo apt-get update
     sudo apt-get -y install nginx
+
+    sudo echo "Hello World!" | sudo tee /var/www/html/index.html
+    sudo sed -i '23i\rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
+    echo "Ceci n'est pas une page" | sudo tee /usr/share/nginx/html/404_page.html
+    sudo sed -i '24i\error_page 404 /404_page.html; location = /404_page.html { root /usr/share/nginx/html/404_page.html; internal;}' /etc/nginx/sites-available/default
+    # Add custom HTTP response header to Nginx
+    sudo sed -i 's|^\(server {\)|\1\n        add_header X-Served-By $hostname;|' /etc/nginx/sites-available/default
 fi
 
 # Create necessary directories
-sudo mkdir -p /data
-sudo mkdir -p /data/web_static
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo chown -R ubuntu:ubuntu /data/
@@ -33,5 +38,3 @@ sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/
 
 # Restart Nginx
 sudo service nginx restart
-
-exit 0
