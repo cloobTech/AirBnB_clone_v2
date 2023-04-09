4ada8d8d7072f6b52fba96764c564e8f1801ba5d
cloobTech <belkid98@gmail.com>
2023-04-09 01:24:00 -0700

    setting

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,73 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+# script thats sets up web servers for deployment of web_static
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-
-    sudo echo "Hello World!" | sudo tee /var/www/html/index.html
-    sudo sed -i '23i\rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
-    echo "Ceci n'est pas une page" | sudo tee /usr/share/nginx/html/404_page.html
-    sudo sed -i '24i\error_page 404 /404_page.html; location = /404_page.html { root /usr/share/nginx/html/404_page.html; internal;}' /etc/nginx/sites-available/default
-    # Add custom HTTP response header to Nginx
-    sudo sed -i 's|^\(server {\)|\1\n        add_header X-Served-By $hostname;|' /etc/nginx/sites-available/default
+#install nginx if it is not installed
+if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
+    sudo apt-get update -y
+    sudo apt-get install nginx -y
 fi
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+#firewall setting: allow nginx to serve on port 80
+sudo ufw allow 'Nginx HTTP'
+
+# replace the content of the served page
+echo "Hello World!" | sudo tee /var/www/html/index.html
+
+# grant user ownership of the web files
+
+sudo chown -R "$USER":"$USER" /var/www/html
+sudo chmod -R 755 /var/www
+
+# redirect /redirect_me to youtube video
+sudo sed -i '38i\rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
+
+#message to be served when file requested is not found
+printf "Ceci n'est pas une page\n" | sudo tee /usr/share/nginx/html/custom_404.html
+
+#configure Nginx server to use error page
+sudo sed -i '40i\error_page 404 /custom_404.html; location = /custom_404.html { root /usr/share/nginx/html; internal;}' /etc/nginx/sites-available/default
+
+# create the folder /data/ if it doesnt exist
+FOLDER_DATA=/data/
+[ -d "$FOLDER_DATA" ] || sudo mkdir /data/
+
+#Create the folder /data/web_static/ if it doesn’t already exist
+FOLDER_WEBSTATIC=/data/web_static/
+[ -d "$FOLDER_WEBSTATIC" ] || sudo mkdir /data/web_static/
+
+#Create the folder /data/web_static/releases/ if it doesn’t already exist
+FOLDER_RELEASES=/data/web_static/releases
+[ -d "$FOLDER_RELEASES" ] || sudo mkdir /data/web_static/releases/
+
+#Create the folder /data/web_static/shared/ if it doesn’t already exist
+FOLDER_SHARED=/data/web_static/shared/
+[ -d "$FOLDER_SHARED" ] || sudo mkdir /data/web_static/shared
+
+#Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+FOLDER_TEST=/data/web_static/releases/test/
+[ -d "$FOLDER_TEST" ] || sudo mkdir /data/web_static/releases/test/
+
+#create a fake html file with small content to be served
+printf "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n" | sudo tee /data/web_static/releases/test/index.html
+
+
+# create a symbolic link to /data/web_static/releases/test/ folder
+FILE_SYMLINK=/data/web_static/current
+if [ -L "$FILE_SYMLINK" ]; then
+    rm /data/web_static/current
+    sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+else
+    sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+fi
+
+#give ownership of /data/ to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Create a fake HTML file
-echo "
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Update Nginx configuration
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-# Restart Nginx
+#update nginx configuration to serve content of /data/web_static/current/ to hbnb_static
+sudo sed -i '74i\location /hbnb_static {alias /data/web_static/current;}' /etc/nginx/sites-available/default
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+    sudo service nginx start
+else
+    sudo service nginx restart
 sudo service nginx restart
+fi
