3e74c562b97a3ba8c8891e6cc5de5ab38786a17f
cloobTech <belkid98@gmail.com>
2023-04-23 12:57:00 -0700

    setting

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  a script that starts a Flask web application: """
+""" a script that starts a Flask web application:"""
 from flask import Flask
 
 
@@ -8,8 +8,14 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ returns a string"""
+    """ return a strin"""
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ return a strin"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
