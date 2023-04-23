59cead9af43d7a959a31cb297f3248b2d5783841
cloobTech <belkid98@gmail.com>
2023-04-23 13:41:10 -0700

    templating

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,5 +42,11 @@ def number_template(n):
     return render_template('number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """display a HTML page if n is int"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
